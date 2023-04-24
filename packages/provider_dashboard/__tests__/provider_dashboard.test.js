fbc9497d69d302b1758f97354d2619c374b55016
komvla <53231792+komvla@users.noreply.github.com>
2023-04-25 01:25:00 +0200

    Revert "Public shop"

--- packages/provider_dashboard/__tests__/provider_dashboard.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const providerDashboard = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(providerDashboard(), 'Hello from providerDashboard');
-console.info('providerDashboard tests passed');
--- packages/public_shop/__tests__/public_shop.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const publicShop = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(publicShop(), 'Hello from publicShop');
-console.info('publicShop tests passed');
